187fffac619dd27e90d34abfe13a3c75094663e0
Wei Liu <weiliu2000@outlook.com>
2022-08-02 13:53:26 +0800

    Create HomeAdapter

---
+++ app/src/main/java/com/example/appproject/ui/home/HomeAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    val data = mutableListOf<ArticleItem>()
+
+    private var onItemClick: ((ArticleItem)->Unit)?=null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+       val view = LayoutInflater.from(parent.context).inflate(R.layout.item,parent ,false)
+        return  object :RecyclerView.ViewHolder(view){}
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val news = data[position]
+        holder.itemView.findViewById<TextView>(R.id.item_title).text = data[position].title
+        holder.itemView.findViewById<TextView>(R.id.item_time).text = data[position].niceDate
+        val iv = holder.itemView.findViewById<ImageView>(R.id.imageView)
+        if (news.collect) {
+            iv.setImageResource(R.drawable.redheart)
+
+        } else {
+            iv.setImageResource(R.drawable.emptyheart)
+        }
+        iv.setOnClickListener {
+            news.collect = !news.collect
+            notifyDataSetChanged()
+        }
+        holder.itemView.setOnClickListener{
+            onItemClick?.invoke(news)
+            it.findViewById<ImageView>(R.id.imageView).setOnClickListener{
+            }
+        }
+    }
+    override fun getItemCount(): Int = data.size
+    fun setOnItemClickListener(listener:(ArticleItem)->Unit){
+        onItemClick = listener
+    }
+}
